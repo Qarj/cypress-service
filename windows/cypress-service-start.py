6ff0b0a70c62a1b94063156bbe08fa84e43e8e95
Tim Buckland <tim.buckland@stepstone.com>
2021-04-13 09:25:26 +0100

    cypress-service bat scripts

--- windows/cypress-service-start.py
+++ windows/cypress-service-start.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 from coreutil import grep, int_grep, shell, cypress_port, project_root, set_log_file, log
-from coreutil import kill_processes_locking_server_log, pid_listening_on_port
+from coreutil import kill_processes_locking_service_log, pid_listening_on_port
 from coreutil import first_pid_locking_server_log, today
 import os
 from time import sleep
 
-def cypress_server_already_running ():
+def cypress_service_already_running ():
     nothingRunningOnPort, netstatSTDOUT = shell(f"netstat -ano | findstr :{cypress_port ()}")
     if nothingRunningOnPort:
         return False
@@ -24,28 +24,28 @@ def server_log_locked ():
     log ('Server log file is locked')
     return True
 
-def start_cypress_server ():
-    start_server_cmd = f'cd /D {project_root ()} && set "CYPRESS_CACHE_FOLDER=C:\CYPRESS_CACHE" && start cmd.exe /c "npm run start > {project_root ()}\\logs\\server_{today ()}.log 2>&1"'
-    os.system (start_server_cmd) # cannot run with subprocess - python will wait
+def start_cypress_service ():
+    start_service_cmd = f'cd /D {project_root ()} && set "CYPRESS_CACHE_FOLDER=C:\CYPRESS_CACHE" && start cmd.exe /c "npm run start > {project_root ()}\\logs\\cypress-service.log 2>&1"'
+    os.system (start_service_cmd) # cannot run with subprocess - python will wait
 
-set_log_file ('cypress_start.log')
+set_log_file ('cypress-service-start.log')
 
-if cypress_server_already_running ():
-    log ('Cypress server is already running - not starting')
+if cypress_service_already_running ():
+    log ('Cypress service is already running - not starting')
     exit (1)
-log ('Cypress server is not running - starting now')
+log ('Cypress service is not running - starting now')
 
 if server_log_locked ():
-    kill_processes_locking_server_log ()
+    kill_processes_locking_service_log ()
 
-start_cypress_server ()
+start_cypress_service ()
 
 for _ in range (5):
-    if cypress_server_already_running ():
-        log ('Cypress server now started')
+    if cypress_service_already_running ():
+        log ('Cypress service now started')
         exit (0)
     sleep (3)
 
-log ('Cypress server failed to start')
+log ('Cypress service failed to start')
 exit (1)
    
